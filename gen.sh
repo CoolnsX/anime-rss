aff8bfbac4e203a367f5e1c2d038a412bcbddc57
coolnsx <ansari.tan20@gmail.com>
2023-01-08 13:20:28 +0530

    Revert this commit

--- gen.sh
+++ gen.sh
@@ -6,7 +6,7 @@ printf '<?xml version="1.0" encoding="UTF-8" ?>\n<rss version="2.0">\n\n<channel
 #content
 for i in $(curl -s "https://allanime.site/anime" -A uwu | sed -nE 's|.*href="(/watch/[^"]*)" class.*|\1|p')
 do
-	printf '\n<item>\n<title>%s</title>\n<link>%s</link>\n<description>A simple RSS feed for allanime!</description>\n</item>\n' "$(printf "%s" "$i" | cut -d'/' -f4- | tr '[:punct:]' ' ' )" "https://allanime.site${i}" >> allanime.xml
+	grep -q "$i" allanime.xml || printf '\n<item>\n<title>%s</title>\n<link>%s</link>\n<description>A simple RSS feed for allanime!</description>\n</item>\n' "$(printf "%s" "$i" | cut -d'/' -f4- | tr '[:punct:]' ' ' )" "https://allanime.site${i}" >> allanime.xml
 done
 
 #footer
