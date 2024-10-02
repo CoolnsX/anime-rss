9a8f09dcd9d6641c5b5085569630537b28cab7f1
coolnsx <ansari.tan20@gmail.com>
2024-10-02 18:45:46 +0530

    I made leetle mistake

--- gen.sh
+++ gen.sh
@@ -11,7 +11,7 @@ currenttime="$(date -Ru)"
 for i in $(curl -e 'https://allanime.to' -s -G "https://api.allanime.day/api" -d "variables=%7B%22search%22%3A%7B%22sortBy%22%3A%22Recent%22%2C%22allowAdult%22%3Afalse%2C%22allowUnknown%22%3Afalse%7D%2C%22limit%22%3A40%2C%22page%22%3A1%2C%22translationType%22%3A%22sub%22%2C%22countryOrigin%22%3A%22JP%22%7D" --data-urlencode "query=$query"  -A "Mozilla/5.0" | sed 's|Show|\n|g' | sed -nE 's|.*_id":"([^"]*)","name":"([^"]*)".*sub":\{"episodeString":"([^"]*)".*|\1\t\2\tepisode \3 sub|p' | tr '[:punct:]' ' ' | tr -s ' ' | tr ' \t' '-/');do
 	title="$(printf "%s" "$i" | cut -d'/' -f2- | tr '[:punct:]' ' ' )"
 	epdate="$(printf '%s' "$oldfile" | grep -A3 "<title>${title}</title>" | grep "<pubDate>" | sed -E 's_<pubDate>([^<>]+)</pubDate>_\1_')"
-	printf '\n<item>\n<title>%s</title>\n<link>%s</link>\n<description>A simple RSS feed for allanime!</description>\n<pubDate>%s</pubDate>\n</item>\n' "$title" "https://allanime.site/watch/$(printf "%s" "$i" | cut -d'/' -f1,3)" "${epdate:-$currenttime}" >> allanime.xml
+	printf '\n<item>\n<title>%s</title>\n<link>%s</link>\n<description>A simple RSS feed for allanime!</description>\n<pubDate>%s</pubDate>\n</item>\n' "$title" "https://allmanga.to/bangumi/$(printf "%s" "$i" | cut -d'/' -f1- | sed 's/episode/p/g')" "${epdate:-$currenttime}" >> allanime.xml
 done
 
 #footer
